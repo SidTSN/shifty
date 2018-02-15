bffa2ff7ba64cd94edc0b0d483d165dff33c9470
SidTSN <mathewstone-cardwell@outlook.com>
2018-02-15 18:32:32 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,37 @@ client.on('ready', () => {
 
 var prefix = "Shifty/"
 client.on('message', message => {
-    if (!message.content.startsWith(prefix)) return;
-    if (message.author.bot) return;
-    if (message.content.startsWith(prefix + 'r u online')) {
-        message.channel.sendMessage('yes, i am online and awaiting command');
-    }
+    let args = message.content.split(' ').slice(1);
+	var result = args.join(' ');
+
+	if (!message.content.startsWith(prefix)) return;
+	if (message.author.bot) return;
+
+	if (message.content.startsWith(prefix + 'r u online')) {
+		message.channel.sendMessage(`Yes, I am online! \`${Date.now() - message.createdTimestamp} ms\``);
+	} else
+
+	if (message.content.startsWith(prefix + 'send')) {
+		client.channels.get('245491978601627648').sendMessage('Hello from second channel!');
+	} else
+
+	if (message.content.startsWith(prefix + 'setgame')) {
+		if (!result) {
+			result = null;
+		}
+		client.user.setGame(result);
+	} else
+
+	if (message.content.startsWith(prefix + 'setstatus')) {
+		if (!result) {
+			result = 'online';
+		}
+		client.user.setStatus(result);
+	} else
+
+	if (message.content.startsWith(prefix + 'What is your prefix?')) {
+		message.channel.sendMessage(prefix);
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY

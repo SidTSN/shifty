b537a0ff8305c5e744590d0ca400091f1f490e5b
SidTSN <mathewstone-cardwell@outlook.com>
2018-02-15 17:41:05 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    if (message.content === 'r u online') {
+        message.reply('yes, i am online and awaiting command');
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY

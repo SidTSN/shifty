8a59779a3d22310277551ad291fc63216b0fc401
SidTSN <mathewstone-cardwell@outlook.com>
2018-02-15 19:10:47 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = require('./settings.json').prefix;
+const config = require("./config.json");
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -11,35 +11,35 @@ client.on('message', message => {
 	var result = args.join(' ');
 
 	if (message.content === 'What is your prefix?') {
-	        message.channel.sendMessage(prefix);
+	        message.channel.sendMessage(config.prefix);
 	}
 	
-	if (!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(config.prefix)) return;
 	if (message.author.bot) return;
 
-	if (message.content.startsWith(prefix + 'r u online')) {
+	if (message.content.startsWith(config.prefix + 'r u online')) {
 		message.channel.sendMessage(`Yes, I am online! \`${Date.now() - message.createdTimestamp} ms\``);
 	} else
 
-	if (message.content.startsWith(prefix + 'send')) {
+	if (message.content.startsWith(config.prefix + 'send')) {
 		client.channels.get('245491978601627648').sendMessage('Hello from second channel!');
 	} else
 
-	if (message.content.startsWith(prefix + 'setgame')) {
+	if (message.content.startsWith(config.prefix + 'setgame')) {
 		if (!result) {
 			result = null;
 		}
 		client.user.setGame(result);
 	} else
 
-	if (message.content.startsWith(prefix + 'setstatus')) {
+	if (message.content.startsWith(config.prefix + 'setstatus')) {
 		if (!result) {
 			result = 'online';
 		}
 		client.user.setStatus(result);
 	} else
 
-	if (message.content.startsWith(prefix + 'foo')) {
+	if (message.content.startsWith(config.prefix + 'foo')) {
 		message.channel.sendMessage('bar');
 	}
 });

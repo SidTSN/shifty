c76e472df8fe24e2c9bf2d2f2f343836c2215c18
SidTSN <mathewstone-cardwell@outlook.com>
2018-02-19 13:26:48 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,25 @@
 const { Client } = require('discord.js');
 const bot = new Client();
 const cfg = require('./config.json');
+//Logs to console on startup and sets bot presence msg
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag} (${bot.user.id}) on ${bot.guilds.size} servers.`);
   bot.user.setPresence({ game: { name: `on ${bot.guilds.size} servers.`, type: 0 } });
 });
 
 bot.on('message', msg => {
+  //removes case-sensitivity and edits msg to give a better format in console
   if (msg.author.bot || !msg.content.startsWith(cfg.prefix)) return;
   const args = msg.content.slice(cfg.prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
-  if (command === 'online?') {
-    message.channel.send('Possibly.');
+  //start of commands
+  if (command === 'online') {
+    bot.sendMessage(message.channel, "yes");
   }
+  //end of commands
+  //logs commands to console
   console.log(`Args: ${args}\nCommand: ${command}`);
 });
 
-// IF YOU CHANGE THIS THEN THE BOT WONT WORK UNLESS YOU ARE SELF-HOSTING THE BOT
+//Bot Token (If hosting on heroku do not change)
 bot.login(process.env.BOT_TOKEN);

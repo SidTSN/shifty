f5aa7e7e33ed2cd3d9f21eafbb0f58e4ad27b1a3
SidTSN <mathewstone-cardwell@outlook.com>
2018-02-22 08:50:38 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,58 @@
+//Constants and variables.
 const { Client } = require('discord.js');
 const bot = new Client();
-const cfg = require('./config.json');
-//Logs to console on startup and sets bot presence msg
-bot.on('ready', () => {
+const cfg = require('./config.json')
+
+//Log to console on boot and sets bot presence.
+bot.on('ready', () => 
+{
   console.log(`Logged in as ${bot.user.tag} (${bot.user.id}) on ${bot.guilds.size} servers.`);
   bot.user.setPresence({ game: { name: `on ${bot.guilds.size} servers.`, type: 0 } });
 });
-// client.on('',''=>{});
-bot.on('guildDelete', guild => {
-  guild.defaultChannel.send(`I have left ${guild.name} at ${new Date()}`);
+
+//Commands.
+bot.on('guildCreate', member => 
+{
+  const channel = member.guild.channels.find('name', 'arrivals');
+  if (!channel) return;
+  channel.send(`${member} has joined the server.`);
 });
 
-bot.on('guildCreate', guild => {
-  guild.defaultChannel.send(`I have joined ${guild.name}`);
+bot.on('guildDelete', guild => 
+{
+  const channel = member.guild.channels.find('name', 'arrivals');
+  if (!channel) return;
+  channel.send(`${member} has left the server.`);
 });
 
-bot.on('message', msg => {
+bot.on('guildMemberAdd', member => 
+{
+  const channel = member.guild.channels.find('name', 'arrivals');
+  if (!channel) return;
+  channel.send(`${member} has joined the server.`);
+});
+
+bot.on('guildMemberRemove', member => 
+{
+  const channel = member.guild.channels.find('name', 'arrivals');
+  if (!channel) return;
+  channel.send(`${member} has left the server.`);
+});
+
+bot.on('message', msg => 
+{
   if (msg.author.bot || !msg.content.startsWith(cfg.prefix)) return;
   const args = msg.content.slice(cfg.prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
   
-  if (command === 'ruonline?') {
+  if (command === 'ruonline?') 
+  {
     msg.channel.send('Pinging...').then(m => {
       m.edit(`Yes \`${Date.now() - msg.createdTimestamp} ms\``);
     });
   }
   console.log(`Args: ${args}\nCommand: ${command}`);
 });
-//Bot Token (If hosting on heroku do not change)
+
+//Bot Token.
 bot.login(process.env.BOT_TOKEN);
